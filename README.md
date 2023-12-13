fe3d18c44206d89a4f00320404f990d33dcd700c
Kourosh Riahidehkordi <Kourosh.Riahidehkordi@iit.it>
2023-12-13 12:09:07 +0100

    added local version

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FFH_PDFViewer
+
+A local version which creates a server on a local machine with Node.js and Express.
---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(express.static('public')); // Serve static files from a directory named 'public'
+
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = __dirname + '/public/pdf'; // Replace with the path to your folder
+const resultArray = [];
+
+const ip = require('ip');
+const port = 3000;
+const networkIPAddress = ip.address(); // Replace with your local network IP address ('192.168.1.5')
+
+app.get("/", function(req, res){
+    res.render("index", {resultArray, num: 6})
+})
+
+
+// Bind the server to your local network IP address
+app.listen(port, networkIPAddress, () => {
+  console.log(`Server is running on http://${networkIPAddress}:${port}`);
+});
+
+// Function to recursively read the contents of a directory and store file names
+function readDirectory(dir) {
+    const contents = fs.readdirSync(dir);
+    contents.forEach((item) => {
+      const itemPath = path.join(dir, item);
+      const stats = fs.statSync(itemPath);
+  
+      if (stats.isDirectory()) {
+        // If it's a directory, recursively read its contents
+        readDirectory(itemPath);
+      } else {
+        // If it's a file, store its name (without the path)
+        resultArray.push(item);
+      }
+    });
+  }
+
+// Call the function to read the directory
+readDirectory(directoryPath);
+
+
+
+
+
+
+  
